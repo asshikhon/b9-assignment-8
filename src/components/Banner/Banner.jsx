4c2fb1cc854d98fda2af4f74554ba3a95341976a
asshikhon <asshikhon@gmail.com>
2024-03-26 00:38:14 +0600

    added banner section

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,17 +3,18 @@ import { Link } from "react-router-dom";
 
 const Banner = () => {
     return (
-        <div className="hero bg-[#1313130d] ">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <img src="../../../src/assets/images/banner.png" className="my-20 rounded-lg" />
+        <div className="hero bg-[#1313130d] rounded-3xl">
+        <div className="hero-content flex-col lg:flex-row">
+         
           <div>
-            <h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
+            <h1 className="text-4xl leading-[80px] lg:text-[56px] text-[#131313] font-bold pb-4 lg:pb-12">Books to freshen up <br /> your bookshelf</h1>
 
             <Link to="/listed">
-            <button className="btn btn-primary">View The List</button>
+            <button className="btn bg-[#23BE0A] border-0 font-work-sans btn-primary text-white font-bold text-lg">View The List</button>
 
             </Link>
           </div>
+          <img src="../../../src/assets/images/banner.png" className="my-10 lg:my-20 rounded-lg lg:ml-24" />
         </div>
       </div>
     );
